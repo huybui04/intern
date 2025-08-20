b4458b627db2cf41fe78daa08d4e76ec4723a788
huybui04 <huyheo27112004@gmail.com>
2025-08-20 10:37:25 +0700

    chore(filter): add boolean case handling in mongo filter

--- src/utils/filterToMongo.ts
+++ src/utils/filterToMongo.ts
@@ -14,6 +14,17 @@ export const filterToMongo = (filterModel: any): any => {
           if (filter.type === "greaterThan")
             mongoFilter[field] = { $gt: filter.filter };
           break;
+        // case "date":
+        //   if (filter.type === "equals")
+        //     mongoFilter[field] = { $eq: new Date(filter.filter) };
+        //   if (filter.type === "lessThan")
+        //     mongoFilter[field] = { $lt: new Date(filter.filter) };
+        //   if (filter.type === "greaterThan")
+        //     mongoFilter[field] = { $gt: new Date(filter.filter) };
+        //   break;
+        case "boolean":
+          mongoFilter[field] = filter.filter;
+          break;
         default:
           break;
       }
